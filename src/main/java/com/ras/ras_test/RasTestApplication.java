37357cbd85729055ca5b35c171b2b6f33a7f61f6
Herbeth Milhome <h2milhome@gmail.com>
2024-05-26 15:43:39 -0300

    init project

---
+++ src/main/java/com/ras/ras_test/RasTestApplication.java
@@ -0,0 +1,13 @@
+package com.ras.ras_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ras/ras_test/RasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ras.ras_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
